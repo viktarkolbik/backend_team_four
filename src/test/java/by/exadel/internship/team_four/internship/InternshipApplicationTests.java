595b8898945e7ee9462002017a772f9d116879d4
Valiantsin Vialichka <velichko1882@gmail.com>
2021-03-23 20:49:50 +0300

    Change .gitignore, delete second internship-package

--- src/test/java/by/exadel/internship/team_four/internship/InternshipApplicationTests.java
+++ src/test/java/by/exadel/internship/team_four/internship/InternshipApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Test2ApplicationTests {
+class InternshipApplicationTests {
 
 	@Test
 	void contextLoads() {
