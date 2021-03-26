76d2c7ad2968d1535fdd610e69d5b2dc787d3606
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2021-03-26 13:12:42 +0300

    created entity Form and Interview (#4)

---
+++ src/main/java/by/exadel/internship/team_four/entity/Interview.java
@@ -0,0 +1,18 @@
+package by.exadel.internship.team_four.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Interview {
+
+    //private User admin;
+    //private Date InterviewDate
+    private String feedback;
+    //private User techSpecialist;
+    //private Date techInterviewDate;
+    private String techFeedback;
+}
