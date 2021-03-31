c4c9e9a4ff3e3b4b98f0cd06789fb019437471f9
ShirshovAlex <58988376+ShirshovAlex@users.noreply.github.com>
2021-03-31 23:24:30 +0300

    Delete src/main/resources/db/changelog/v.0.1/sqlFile directory

--- src/main/resources/db/changelog/v.0.1/sqlFile/insert_form.sql
+++
@@ -1,7 +0,0 @@
-CREATE extension IF NOT EXISTS "uuid-ossp";
-INSERT INTO form (fm_id, fm_name, fm_lastname, fm_midlename, fm_email, fm_phone_number, fm_skype, fm_country, fm_city, fm_experience, fm_education, fm_file_path, fm_primary_skill)
-VALUES (uuid_generate_v4(),'testName','testLastName','testMiddleName','testEmail','testPhone','testSkype','testCountry','testCity','testExp','testEducation','testPATH','testSkills');
-INSERT INTO form (fm_id, fm_name, fm_lastname, fm_midlename, fm_email, fm_phone_number, fm_skype, fm_country, fm_city, fm_experience, fm_education, fm_file_path, fm_primary_skill)
-VALUES (uuid_generate_v4(),'testName','testLastName','testMiddleName','testEmail','testPhone','testSkype','testCountry','testCity','testExp','testEducation','testPATH','testSkills');
-INSERT INTO form (fm_id, fm_name, fm_lastname, fm_midlename, fm_email, fm_phone_number, fm_skype, fm_country, fm_city, fm_experience, fm_education, fm_file_path, fm_primary_skill)
-VALUES (uuid_generate_v4(),'testName','testLastName','testMiddleName','testEmail','testPhone','testSkype','testCountry','testCity','testExp','testEducation','testPATH','testSkills');
