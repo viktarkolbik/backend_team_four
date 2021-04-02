a085fb9f0bf35be17af17ac78d79c84b32607bec
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2021-04-02 17:33:41 +0300

    created entity

---
+++ src/main/java/by/exadel/internship/entity/Internship.java
@@ -0,0 +1,55 @@
+package by.exadel.internship.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "internship")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Internship {
+
+    @Id
+    @Column(name = "inship_id")
+    private UUID id;
+    @Column(name = "inship_name")
+    private String name;
+    @Column(name = "inship_start_date")
+    private LocalDate startDate;
+    @Column(name = "inship_end_date")
+    private LocalDate endDate;
+    @Column(name = "inship_publication_date")
+    private LocalDate publicationDate;
+    @Column(name = "inship_tech_skills")
+    private String techSkills;
+    @Column(name = "inship_description")
+    private String description;
+    @Column(name = "inship_requirements")
+    private String requirements;
+    @Column(name = "inship_capacity")
+    private Integer capacity;
+    @Column(name = "inship_registration_start_date")
+    private LocalDate registrationStartDate;
+    @Column(name = "inship_registration_end_date")
+    private LocalDate registrationEndDate;
+//    private List<FormFullDTO> formList;
+//    private List<UserDTO> techList;
+//    private List<UserDTO> adminList;
+//    private List<LocationDTO> countryList;
+//    private InternshipFormat InternshipFormat;
+//    private List<Technology> technologyList;
+
+
+}
