34dc3102581b5236f8c699bec2fd040c6b2e7b87
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2021-04-02 20:35:38 +0300

    added @CrossOrigin

--- src/main/java/by/exadel/internship/controller/InternshipController.java
+++ src/main/java/by/exadel/internship/controller/InternshipController.java
@@ -6,14 +6,12 @@ import by.exadel.internship.service.InternshipService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/internships")
 @Api(tags = "Endpoints for Internship")
