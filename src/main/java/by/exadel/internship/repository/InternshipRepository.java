d69437fc46df4d1a44b7cf8b5e77ee1c1d3bbebe
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2021-04-02 17:47:53 +0300

    Added Service and Repository Internship

---
+++ src/main/java/by/exadel/internship/repository/InternshipRepository.java
@@ -0,0 +1,8 @@
+package by.exadel.internship.repository;
+
+import by.exadel.internship.entity.Internship;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface InternshipRepository extends JpaRepository<Internship, UUID> {}
---
+++ src/main/java/by/exadel/internship/service/InternshipService.java
@@ -0,0 +1,48 @@
+package by.exadel.internship.service;
+
+
+import by.exadel.internship.dto.internshipDTO.GuestInternshipDTO;
+import by.exadel.internship.entity.Internship;
+import by.exadel.internship.mapper.InternShipMapper;
+import by.exadel.internship.repository.InternshipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class InternshipService {
+
+    private final InternshipRepository internshipRepository;
+    private final InternShipMapper internShipMapper;
+
+    @Autowired
+    public InternshipService(InternshipRepository internshipRepository, InternShipMapper internShipMapper) {
+        this.internshipRepository = internshipRepository;
+        this.internShipMapper = internShipMapper;
+    }
+
+
+    public GuestInternshipDTO getById(UUID uuid) {
+
+        Internship internship = internshipRepository.getOne(uuid);
+        GuestInternshipDTO guestInternshipDTO = internShipMapper.toGuestInternshipDTO(internship);
+
+        return guestInternshipDTO;
+    }
+
+    public List<GuestInternshipDTO> getAll(){
+
+        List<Internship> internships = internshipRepository.findAll();
+        List<GuestInternshipDTO> guestInternshipDTOList = new ArrayList<>();
+        for (Internship i: internships) {
+            guestInternshipDTOList.add(internShipMapper.toGuestInternshipDTO(i));
+        }
+
+        return guestInternshipDTOList;
+    }
+
+
+}
