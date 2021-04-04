b2a99202b6bbcad8601be943f8884803d469bee3
Valiantsin Vialichka <velichko1882@gmail.com>
2021-04-05 00:41:47 +0300

    add add docker-compose.yaml, change application.yaml, InternshipApplicationTests.java, pom.xml

--- src/test/java/by/exadel/internship/team_four/InternshipApplicationTests.java
+++ src/test/java/by/exadel/internship/team_four/InternshipApplicationTests.java
@@ -1,9 +1,7 @@
 package by.exadel.internship.team_four;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class InternshipApplicationTests {
 
 	@Test
