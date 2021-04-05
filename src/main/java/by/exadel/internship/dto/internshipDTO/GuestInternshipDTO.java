d5516a1e24af486deb7b1811eb9dc365f2af4657
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2021-04-05 10:22:24 +0300

    example of validation for GuestInternshipDTO

--- src/main/java/by/exadel/internship/dto/internshipDTO/GuestInternshipDTO.java
+++ src/main/java/by/exadel/internship/dto/internshipDTO/GuestInternshipDTO.java
@@ -9,6 +9,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
@@ -20,17 +22,30 @@ import java.util.UUID;
 public class GuestInternshipDTO {
 
     private UUID id;
+    @Size(min=2, max=30)
     private String name;
+    @NotNull
     private LocalDate startDate;
+    @NotNull
     private LocalDate endDate;
+    @NotNull
     private LocalDate publicationDate;
+    @Size(max=150)
     private String techSkills;
+    @NotNull
     private List<LocationDTO> countryList;
+    @NotNull
     private InternshipFormat InternshipFormat;
+    @NotNull
     private List<Technology> technologyList;
+    @Size(max=150)
     private String description;
+    @Size(max=150)
     private String requirements;
+    @NotNull
     private Integer capacity;
+    @NotNull
     private LocalDate registrationStartDate;
+    @NotNull
     private LocalDate registrationEndDate;
 }
