3bc0bfc21183eb68110f65da833fd5a45146324d
Olga <olgaaevdokim@gmail.com>
2021-04-08 17:28:05 +0300

    uncomment simpleFormMapperTest

--- src/test/java/by/exadel/internship/mapper/SimpleFormMapperTest.java
+++ src/test/java/by/exadel/internship/mapper/SimpleFormMapperTest.java
@@ -1,34 +1,34 @@
 package by.exadel.internship.mapper;
-//
-//import by.exadel.internship.dto.formDTO.FormFullDTO;
-//import by.exadel.internship.entity.Form;
-//import org.junit.jupiter.api.Test;
-//import org.mapstruct.factory.Mappers;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//public class SimpleFormMapperTest {
-//    private FormMapper mapper
-//            = Mappers.getMapper(FormMapper.class);
-//
-//    @Test
-//    public void givenEntityToDTO_whenMaps_thenCorrect() {
-//        Form form = new Form();
-//        form.setCity("minsk");
-//        form.setCountry("Belarus");
-//        FormFullDTO formFullDTO = mapper.toFormDto(form);
-//
-//        assertEquals(form.getCity(), formFullDTO.getCity());
-//        assertEquals(form.getCountry(), formFullDTO.getCountry());
-//    }
-//
-//    @Test
-//    public void givenDTOtoEntity_whenMaps_thenCorrect() {
-//        FormFullDTO formFullDTO = new FormFullDTO();
-//        formFullDTO.setEmail("testEmail");
-//        formFullDTO.setEducation("testEducation");
-//        Form form = mapper.toFormEntity(formFullDTO);
-//
-//        assertEquals(formFullDTO.getEducation(), form.getEducation());
-//        assertEquals(formFullDTO.getEmail(), form.getEmail());
-//    }
-//}
+import by.exadel.internship.dto.formDTO.FormFullDTO;
+import by.exadel.internship.entity.Form;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleFormMapperTest {
+    private FormMapper mapper
+            = Mappers.getMapper(FormMapper.class);
+
+    @Test
+    public void givenEntityToDTO_whenMaps_thenCorrect() {
+        Form form = new Form();
+        form.setCity("minsk");
+        form.setCountry("Belarus");
+        FormFullDTO formFullDTO = mapper.toFormDto(form);
+
+        assertEquals(form.getCity(), formFullDTO.getCity());
+        assertEquals(form.getCountry(), formFullDTO.getCountry());
+    }
+
+    @Test
+    public void givenDTOtoEntity_whenMaps_thenCorrect() {
+        FormFullDTO formFullDTO = new FormFullDTO();
+        formFullDTO.setEmail("testEmail");
+        formFullDTO.setEducation("testEducation");
+        Form form = mapper.toFormEntity(formFullDTO);
+
+        assertEquals(formFullDTO.getEducation(), form.getEducation());
+        assertEquals(formFullDTO.getEmail(), form.getEmail());
+    }
+}
