95f4ed177d62fd02d60e85e2ee34c55567cfb043
ShirshovAlex <shirshovalex547@gmail.com>
2021-04-08 18:11:26 +0300

    add APPLICATION_JSON_VALUE in consumes

--- src/main/java/by/exadel/internship/controller/FormController.java
+++ src/main/java/by/exadel/internship/controller/FormController.java
@@ -8,9 +8,13 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RestController
@@ -21,7 +25,7 @@ public class FormController {
 
     private final FormService formService;
 
-    @PostMapping(consumes = MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(consumes = {MULTIPART_FORM_DATA_VALUE, APPLICATION_JSON_VALUE})
     @ApiOperation("Add new form")
     public ResponseEntity<Form> addNewForm(@RequestPart(name = "form") FormRegisterDTO form,
                                            @RequestPart(name = "file", required = false) MultipartFile file) {
