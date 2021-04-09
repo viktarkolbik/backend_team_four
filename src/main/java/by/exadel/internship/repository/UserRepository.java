48a58b19f7a58f5d83e886c225b1c9f636d0b479
OlgaEvdokimova <61596282+OlgaEvdokimova@users.noreply.github.com>
2021-04-09 14:51:47 +0300

    017create entity user (#18)
    * create User entity
    
    * edit mapping
    
    * user without tech user
    
    * add inheritance and userRole.class
    
    * uncomment simpleFormMapperTest
    
    * Delete ingeritance in tech user, delete column u_type, create type USER_ROLE in liquibase
    
    * Drop TechUser and empty line

---
+++ src/main/java/by/exadel/internship/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package by.exadel.internship.repository;
+import by.exadel.internship.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
