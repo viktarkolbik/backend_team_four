32262f44f502426b318b7f163347d29aa531a424
Olga <olgaaevdokim@gmail.com>
2021-04-19 00:59:29 +0300

    create tables countries and cities, move json to DB

--- src/main/java/by/exadel/internship/InternshipApplication.java
+++ src/main/java/by/exadel/internship/InternshipApplication.java
@@ -9,7 +9,7 @@ public class InternshipApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InternshipApplication.class, args);
-        Parser.parseTo();
+        Parser.putDataToDatabase();
     }
 
 }
--- src/main/java/by/exadel/internship/location/Parser.java
+++ src/main/java/by/exadel/internship/location/Parser.java
@@ -1,28 +1,67 @@
 package by.exadel.internship.location;
 
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.json.simple.JSONArray;
+
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 @Slf4j
 public class Parser {
-     public static void parseTo(){
-         try {
-             JSONParser jsonParser = new JSONParser();
-             JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("by/exadel/internship/location/world-cities_json.json"));
-             JSONArray countryList = (JSONArray) jsonObject.get("country");
-             Iterator<JSONObject> iterator = countryList.iterator();
-             while (iterator.hasNext()){
-                 System.out.println(iterator.next());
-             }
 
-         } catch (ParseException | IOException e) {
-             log.error(e.getMessage());
-         }
-     }
+    public static Map<Object, Set<Object>> getCountriesAndCitiesFromJSON() {
+        Map<Object, Set<Object>> locationMap = new TreeMap<>();
+        try {
+            Object obj = new JSONParser().parse(new FileReader("src/main/java/by/exadel/internship/location/world-cities_json.json"));
+            JSONArray jsonObjectList = (JSONArray) obj;
+            for (Object o : jsonObjectList) {
+                JSONObject jsonObject = (JSONObject) o;
+                Object country = jsonObject.get("country");
+                Object city = jsonObject.get("name");
+                if (!locationMap.containsKey(country)) {
+                    locationMap.put(country, new TreeSet<>());
+                }
+                locationMap.get(country).add(city);
+            }
+        } catch (ParseException | IOException e) {
+            log.error(e.getMessage());
+        }
+        return locationMap;
+    }
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/internship";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "mama071991";
+
+    public static void executeUpdate(String sql) throws SQLException {
+        Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        if (conn == null)
+            throw new SQLException("Connection null!");
+
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(sql);
+        statement.close();
+        conn.close();
+    }
+
+    public static void putDataToDatabase()  {
+        for (Map.Entry<Object, Set<Object>> entry : getCountriesAndCitiesFromJSON().entrySet()) {
+            try {
+                executeUpdate("INSERT INTO country (cntr_name) VALUES(" + "'" +entry.getKey()+ "'" + ");");
+            } catch (SQLException e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
 }
