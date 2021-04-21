f6d1acf843e3f659ac96251964010102125fded0
Fenydude <61800367+Fenydude@users.noreply.github.com>
2021-04-21 12:15:41 +0300

    029-Spring Security JWT (#27)
    * setup config files
    
    * Authorization without password encryption
    
    * Authorization without password encryption
    
    * Fix some mistakes
    
    * Rename auth mapping, delete sout
    
    * remove imports
    
    * Add swagger to white list
    
    * Split email on login and email part
    
    * Fix some mistakes
    
    * Set BCryptPasswordEncoder
    
    * Fix mistakes
    
    * Fix null part1
    
    * Try to add Optional
    
    * Change exception class
    
    * split multiplies method, and do emailSeparator like local variable
    
    * change return type in parseJwt
    
    * use StringUtils in splitEmail and do some local variable constant
    
    * for comment
    
    * Add BadCredentialsException Handler
    
    * optimize imports
    
    * without file
    
    * set UUID into constructor IncorrectData
    
    * do method in GlobalExceptionHandler
    
    * rename PostMapping, and delete unused list of roles
    
    * add setting in Dockerfile

---
+++ src/main/java/by/exadel/internship/config/WebSecurityConfig.java
@@ -0,0 +1,84 @@
+package by.exadel.internship.config;
+
+import by.exadel.internship.config.jwt.AuthEntryPointJwt;
+import by.exadel.internship.config.jwt.AuthTokenFilter;
+import by.exadel.internship.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs",
+    };
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    private final AuthEntryPointJwt unauthorizedHandler;
+
+    private final AuthTokenFilter authTokenFilter;
+
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors()
+                .and()
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(AUTH_WHITELIST);
+    }
+}
