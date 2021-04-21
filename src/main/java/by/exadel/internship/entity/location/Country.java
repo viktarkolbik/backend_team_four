c813eefdcf30d261c6f5820f1a955c68903416ab
OlgaEvdokimova <61596282+OlgaEvdokimova@users.noreply.github.com>
2021-04-21 16:35:51 +0300

    035 location (#33)
    * parse json countries
    
    * create tables countries and cities, move json to DB
    
    * for check
    
    * create DB countries cities
    
    * edit location:
    
    * newMap
    
    * create and populate database from json file
    
    * edit path for world_cities_json.json
    
    * delete unnesessary lines
    
    * edit pom.xml
    
    * edit pom.xml
    
    * create database Location with sql import
    
    * delete unnesessary lines:
    
    * remove unnesessary files'
    
    * edit comments
    
    * add unique constraint to country
    
    * edit name of unique constraint for country name

---
+++ src/main/java/by/exadel/internship/entity/location/Country.java
@@ -0,0 +1,34 @@
+package by.exadel.internship.entity.location;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Data
+@EqualsAndHashCode(exclude = "cityList")
+@Table(name = "country")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Country {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "cntr_id")
+    private UUID id;
+
+    @Column(name = "cntr_name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "country")
+    private Set<City> cityList;
+}
