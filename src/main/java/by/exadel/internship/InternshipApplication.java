9750f5e17e5b0afe6e2ecd9cb13152acea44d88b
Olga <olgaaevdokim@gmail.com>
2021-04-21 12:04:38 +0300

    delete unnesessary lines:

--- src/main/java/by/exadel/internship/InternshipApplication.java
+++ src/main/java/by/exadel/internship/InternshipApplication.java
@@ -1,12 +1,8 @@
 package by.exadel.internship;
 
-import by.exadel.internship.service.location.CityService;
-import by.exadel.internship.service.location.CountryService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication
@@ -14,10 +10,5 @@ public class InternshipApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InternshipApplication.class, args);
-
     }
-
-
-
-
 }
