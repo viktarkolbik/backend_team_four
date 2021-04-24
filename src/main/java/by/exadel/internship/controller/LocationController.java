617fe44b4ef54da8dccb78b5feb2a83c4c96c8a4
Olga <olgaaevdokim@gmail.com>
2021-04-24 13:01:56 +0300

    edit endpoit getCityList()

--- src/main/java/by/exadel/internship/controller/LocationController.java
+++ src/main/java/by/exadel/internship/controller/LocationController.java
@@ -23,9 +23,9 @@ public class LocationController {
         return locationService.getAllCountries();
     }
 
-    @GetMapping("/cities/list-cities-by-country-name/{countryName}")
+    @GetMapping("/cities/country-name-cities")
     @ApiOperation("return city list")
-    public List<CityDTO> getCityList(@PathVariable String countryName){
-        return locationService.getCityListByCountryName(countryName);
+    public List<CityDTO> getCityList(@RequestParam(value = "name") String name){
+        return locationService.getCityListByCountryName(name);
     }
 }
