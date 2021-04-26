4f8854823298805f1f6d8d8fd7b4171a3a822282
Olga <olgaaevdokim@gmail.com>
2021-04-26 13:02:29 +0300

    drop unnessessary imports

--- src/main/java/by/exadel/internship/controller/LocationController.java
+++ src/main/java/by/exadel/internship/controller/LocationController.java
@@ -6,15 +6,16 @@ import by.exadel.internship.service.LocationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/location")
-//@PreAuthorize("hasauthorize('ADMIN')")
 @Api(tags = "Endpoints for Location")
 public class LocationController {
     private final LocationService locationService;
