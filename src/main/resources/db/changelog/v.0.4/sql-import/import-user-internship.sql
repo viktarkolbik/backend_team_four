d8a64d51a6c2f034ade3c0ab552449e34034a12c
ShirshovAlex <shirshovalex547@gmail.com>
2021-04-26 16:25:38 +0300

    resolve conflicts

---
+++ src/main/resources/db/changelog/v.0.4/sql-import/import-user-internship.sql
@@ -0,0 +1,14 @@
+INSERT INTO "user_internship" (ui_u_id, ui_inship_id)
+VALUES ('b64b3afc-b1be-4c7a-9406-d7d14f436332', '68a051d7-6d82-4879-b0a3-1340e14db54d') ON CONFLICT (ui_u_id, ui_inship_id) DO NOTHING;
+INSERT INTO "user_internship" (ui_u_id, ui_inship_id)
+VALUES ('b64b3afc-b1be-4c7a-9406-d7d14f436332', '68a051d7-6d82-4879-b0a3-1340e14db54d') ON CONFLICT (ui_u_id, ui_inship_id) DO NOTHING;
+INSERT INTO "user_internship" (ui_u_id, ui_inship_id)
+VALUES ('740f8b07-3d75-4312-8ba9-1b8531f3940b', '55fdfdd2-cac7-4851-a0a6-ba93113e926b') ON CONFLICT (ui_u_id, ui_inship_id) DO NOTHING;
+INSERT INTO "user_internship" (ui_u_id, ui_inship_id)
+VALUES ('7ae22ab0-93f9-4be5-9adb-4dbd5bf76c5c', '0137afd2-42ca-49e4-9018-3499199fdf43') ON CONFLICT (ui_u_id, ui_inship_id) DO NOTHING;
+INSERT INTO "user_internship" (ui_u_id, ui_inship_id)
+VALUES ('20981ffb-2aa8-41fe-a631-2c6f225bb1e1', '0137afd2-42ca-49e4-9018-3499199fdf43') ON CONFLICT (ui_u_id, ui_inship_id) DO NOTHING;
+INSERT INTO "user_internship" (ui_u_id, ui_inship_id)
+VALUES ('e4ec8eef-c659-46f5-bde8-63551a980553', '967fdef2-18cd-4e77-aef0-7c831cd8263d') ON CONFLICT (ui_u_id, ui_inship_id) DO NOTHING;
+INSERT INTO "user_internship" (ui_u_id, ui_inship_id)
+VALUES ('e4ec8eef-c659-46f5-bde8-63551a980553', '1069033e-e317-407f-be0a-3adc99ae844c') ON CONFLICT (ui_u_id, ui_inship_id) DO NOTHING;
