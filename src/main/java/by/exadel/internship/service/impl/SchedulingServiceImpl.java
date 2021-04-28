c0611004fc851d51932bd321552992f7b217f85c
Fenydude <bogurov1@gmail.com>
2021-04-28 20:19:24 +0300

    update scheduling

--- src/main/java/by/exadel/internship/service/impl/SchedulingServiceImpl.java
+++ src/main/java/by/exadel/internship/service/impl/SchedulingServiceImpl.java
@@ -1,7 +1,6 @@
 package by.exadel.internship.service.impl;
 
 import by.exadel.internship.dto.InterviewDTO;
-import by.exadel.internship.dto.TimeForCallDTO;
 import by.exadel.internship.dto.TimeForCallUserDTO;
 import by.exadel.internship.dto.UserDTO;
 import by.exadel.internship.dto.enums.FormStatus;
@@ -14,18 +13,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class SchedulingServiceImpl implements SchedulingService {
-    private final TimeForCallService timeForCallService;
     private final TimeForCallUserServise timeForCallUserServise;
     private final FormService formService;
     private final UserService userService;
+
+    private static final Map<FormStatus, UserRole> STATUS_USER_ROLE_MAP = new HashMap<>() {
+        {
+            put(FormStatus.REGISTERED, UserRole.ADMIN);
+            put(FormStatus.ADMIN_INTERVIEW_PASSED, UserRole.TECH_EXPERT);
+        }
+    };
 
     private static final String SIMPLE_CLASS_NAME = SchedulingService.class.getSimpleName();
 
@@ -40,25 +43,19 @@ public class SchedulingServiceImpl implements SchedulingService {
 
     private List<TimeForCallUserDTO> checkInterviewOver(FormFullDTO formFullDTO) {
         FormStatus status = formFullDTO.getFormStatus();
-        if (status.name().equals("REGISTERED")){
-            return getTimeForInterviewWithHR();
+        if (status.name().equals("REGISTERED")) {
+            return findAdminFreeTimeForInterview(status);
         }
-        if (status.name().equals("ADMIN_INTERVIEW_PASSED")){
-            System.out.println("ADMIN_INTERVIEW_PASSED form");
+        if (status.name().equals("ADMIN_INTERVIEW_PASSED")) {
+            return findAdminFreeTimeForInterview(status);
         }
-        if (!status.name().equals("REGISTERED") && !status.name().equals("ADMIN_INTERVIEW_PASSED")){
-            System.out.println("Cancel form");
-        }
-        throw new NotFoundException("No such form status for Form with id = " + formFullDTO.getId(),
-                "form.formStatus.invalid");
+        throw new NotFoundException("Form with uuid = " + formFullDTO.getId()
+                + " doesn't need to get users time because Form has "
+                + formFullDTO.getFormStatus() + " status");
     }
 
-    private List<TimeForCallUserDTO> getTimeForInterviewWithHR() {
-        return findHRFreeTimeForInterview();
-    }
-
-    private List<TimeForCallUserDTO> findHRFreeTimeForInterview() {
-        List<UserDTO> users = userService.getAllByUserRole(UserRole.ADMIN);
+    private List<TimeForCallUserDTO> findAdminFreeTimeForInterview(FormStatus formStatus) {
+        List<UserDTO> users = userService.getAllByUserRole(STATUS_USER_ROLE_MAP.get(formStatus));
         List<TimeForCallUserDTO> timeForCallUserDTOList = new ArrayList<>();
         users.forEach(userDTO -> {
             timeForCallUserDTOList.addAll(timeForCallUserServise.getAllByUserId(userDTO.getId()));
@@ -80,12 +77,25 @@ public class SchedulingServiceImpl implements SchedulingService {
         MDCLog.putClassNameInMDC(SIMPLE_CLASS_NAME);
         log.info("Try to save Form with Interview");
         FormFullDTO formFullDTO = formService.getById(formId);
-        InterviewDTO interviewDTO = new InterviewDTO();
-        interviewDTO.setAdmin(userDataTime.getUserId());
-        interviewDTO.setAdminInterviewDate(userDataTime.getStartHour());
-        formFullDTO.setInterview(interviewDTO);
-        formFullDTO.setFormStatus(FormStatus.ADMIN_INTERVIEW_ASSIGNED);
-        formService.updateForm(formFullDTO);
+        if (formFullDTO.getInterview() != null &&
+                formFullDTO.getFormStatus().equals(FormStatus.ADMIN_INTERVIEW_PASSED)) {
+            InterviewDTO interviewDTO = formFullDTO.getInterview();
+            interviewDTO.setTechSpecialist(userDataTime.getUserId());
+            interviewDTO.setTechInterviewDate(userDataTime.getStartHour());
+            formFullDTO.setFormStatus(FormStatus.TECH_INTERVIEW_ASSIGNED);
+            formService.updateForm(formFullDTO);
+            return;
+        }
+        if (formFullDTO.getFormStatus().equals(FormStatus.REGISTERED)) {
+            InterviewDTO interviewDTO = new InterviewDTO();
+            interviewDTO.setAdmin(userDataTime.getUserId());
+            interviewDTO.setAdminInterviewDate(userDataTime.getStartHour());
+            formFullDTO.setInterview(interviewDTO);
+            formFullDTO.setFormStatus(FormStatus.ADMIN_INTERVIEW_ASSIGNED);
+            formService.updateForm(formFullDTO);
+            return;
+        }
+        throw new NotFoundException("Form with uuid = " + formFullDTO.getId() + " doesn't has need Status");
     }
 
 }
