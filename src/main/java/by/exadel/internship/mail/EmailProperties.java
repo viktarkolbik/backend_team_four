b7f2f4b1f7853294090183e9740744cfe116f199
ShirshovAlex <shirshovalex547@gmail.com>
2021-04-29 13:59:36 +0300

    add EmailConfiguration

--- src/main/java/by/exadel/internship/mail/EmailProperties.java
+++ src/main/java/by/exadel/internship/mail/EmailProperties.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,11 +13,17 @@ import org.springframework.context.annotation.Configuration;
 @AllArgsConstructor
 @NoArgsConstructor
 @Configuration
-@ConfigurationProperties(prefix = "email")
+@ConfigurationProperties(prefix = "spring.mail")
 public class EmailProperties {
     private String host;
     private int port;
     private String username;
     private String password;
+    private String protocol;
+    @Value("${spring.mail.smtp.auth}")
+    private String smtpAuth;
+    @Value("${spring.mail.smtp.starttls.enable}")
+    private String smtpStarttls;
+    private String debug;
 
 }
--- src/main/java/by/exadel/internship/service/impl/EmailServiceImpl.java
+++ src/main/java/by/exadel/internship/service/impl/EmailServiceImpl.java
@@ -1,7 +1,6 @@
 package by.exadel.internship.service.impl;
 
 import by.exadel.internship.dto.formDTO.FormRegisterDTO;
-import by.exadel.internship.mail.EmailProperties;
 import by.exadel.internship.mail.EmailTemplate;
 import by.exadel.internship.service.EmailService;
 import lombok.RequiredArgsConstructor;
@@ -11,36 +10,12 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import java.util.Map;
-import java.util.Properties;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class EmailServiceImpl implements EmailService {
 
-    private final EmailProperties emailProperties;
-    private JavaMailSenderImpl mailSender;
-
-    private static final Map<String,String> MAIL_PROPS = Map.of("mail.transport.protocol", "smtp",
-                                                                "mail.smtp.auth", "true",
-                                                                "mail.smtp.starttls.enable", "true",
-                                                                "mail.debug", "true");
-
-    @PostConstruct
-    public void init() {
-        mailSender = new JavaMailSenderImpl();
-
-        mailSender.setHost(emailProperties.getHost());
-        mailSender.setPort(emailProperties.getPort());
-        mailSender.setUsername(emailProperties.getUsername());
-        mailSender.setPassword(emailProperties.getPassword());
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.putAll(MAIL_PROPS);
-
-    }
+    private final JavaMailSenderImpl mailSender;
 
     public boolean sendFormSubmissionEmail(FormRegisterDTO formRegisterDTO) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -53,7 +28,7 @@ public class EmailServiceImpl implements EmailService {
         try {
             mailSender.send(message);
             return true;
-        }catch (MailException e){
+        } catch (MailException e) {
             log.error(e.getMessage());
             return false;
         }
