fedaaa53759b41d44dd3d2327bf6fdc6552d6d00
Olga <olgaaevdokim@gmail.com>
2021-04-29 12:56:02 +0300

    delete unnesser importЭ

--- src/main/java/by/exadel/internship/repository/location/CityRepository.java
+++ src/main/java/by/exadel/internship/repository/location/CityRepository.java
@@ -4,7 +4,6 @@ import by.exadel.internship.entity.location.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface CityRepository extends JpaRepository<City, UUID> {
