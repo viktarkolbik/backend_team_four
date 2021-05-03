1be2d9a133332885bd5c8ac129798813dc982975
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2021-05-03 10:43:21 +0300

    reformat code

--- src/main/java/by/exadel/internship/config/JpaAuditingConfiguration.java
+++ src/main/java/by/exadel/internship/config/JpaAuditingConfiguration.java
@@ -17,7 +17,7 @@ public class JpaAuditingConfiguration {
     @Bean
     public AuditorAware<String> auditorProvider() {
 
-        return () -> Optional.of(((UserDetails) SecurityContextHolder.getContext()
+        return () -> Optional.ofNullable(((UserDetails) SecurityContextHolder.getContext()
                 .getAuthentication().getPrincipal()).getUsername());
     }
 }
