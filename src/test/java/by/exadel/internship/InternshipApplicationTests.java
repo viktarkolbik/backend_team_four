ba0331024a2efe497f1d3481fcb24d276d55dca2
Olga <olgaaevdokim@gmail.com>
2021-05-04 16:17:33 +0300

    edit

--- src/test/java/by/exadel/internship/InternshipApplicationTests.java
+++ src/test/java/by/exadel/internship/InternshipApplicationTests.java
@@ -1,5 +1,6 @@
 package by.exadel.internship;
 
+import by.exadel.internship.config.jwt.JwtService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,10 @@ import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -20,6 +25,8 @@ import org.testcontainers.containers.PostgreSQLContainer;
 
 import javax.annotation.PostConstruct;
 import java.net.URI;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @ContextConfiguration(initializers = {InternshipApplicationTests.Initializer.class})
 @SpringBootTest
@@ -40,20 +47,44 @@ public class InternshipApplicationTests {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private String jwtToken;
+
     @PostConstruct
     public void setTimeModuleUp() {
         objectMapper.registerModule(new JavaTimeModule());
+        jwtToken = generateJWTToken();
+    }
+
+
+    private String generateJWTToken() {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(
+                        "maevsky",
+                        "1"));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return jwtService.generateJwtToken(authentication);
     }
 
     @BeforeEach
     public void setup() throws Exception {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+                .apply(springSecurity())
+                .build();
     }
 
+
     protected MvcResult getResult(HttpMethod method, URI uri, ResultMatcher status) throws Exception {
-        return mockMvc.perform(MockMvcRequestBuilders.request(method, uri))
+        return mockMvc.perform(MockMvcRequestBuilders
+                .request(method, uri).header("Authorization", "Bearer " + jwtToken))
                 .andExpect(status)
                 .andReturn();
+
     }
 
     public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
