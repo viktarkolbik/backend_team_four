f21954ac2b3c22ec5a83ad42a779278cb52c325f
Olga <olgaaevdokim@gmail.com>
2021-05-05 11:44:53 +0300

    resolve conflicts

--- src/test/java/by/exadel/internship/internshipTest/InternshipTest.java
+++ src/test/java/by/exadel/internship/internshipTest/InternshipTest.java
@@ -33,8 +33,6 @@ public class InternshipTest extends InternshipApplicationTests {
     @Autowired
     private InternshipRepository internshipRepository;
 
-
-
     @Test
     public void checkInternshipsTestData() throws Exception {
 
@@ -46,7 +44,7 @@ public class InternshipTest extends InternshipApplicationTests {
         LocationDTO locationDTO1 = new LocationDTO(new CountryDTO(UUID.fromString("21aab798-2d48-459c-bb84-789d2237f933"), "Belarus"), new CityDTO(UUID.fromString("8c48af72-358a-49d4-a786-ce723eefc384"), "Minsk"));
         LocationDTO locationDTO2 = new LocationDTO(new CountryDTO(UUID.fromString("21aab798-2d48-459c-bb84-789d2237f933"), "Belarus"), new CityDTO(UUID.fromString("b38d8113-d5e8-4967-b8ff-e1df0844790f"), "Gomel"));
         LocationDTO locationDTO3 = new LocationDTO(new CountryDTO(UUID.fromString("de5e7623-c298-4033-8419-9e2fd12afdfa"), "Ukraine"), new CityDTO(UUID.fromString("dceb0743-e262-42b8-9d58-bbdb4e5cd729"), "Kiev"));
-        assertEquals(guestFullInternshipDTO.getName(), "Free internship");
+        assertEquals(guestFullInternshipDTO.getName(), "JAVA, JS internship");
         assertEquals(guestFullInternshipDTO.getDescription(), "During the training, you will learn how to write simple and \"clean\" code, create programs that run in a web browser and have web services, develop server-side applications for forums, online stores or surveys, and create multifunctional applications");
         assertEquals(guestFullInternshipDTO.getRequirements(), "OOP knowledge: classes, encapsulation, polymorphism, inheritance;\n" +
                 "knowledge of the basics of SQL and the basics of database design is desirable;\n" +
