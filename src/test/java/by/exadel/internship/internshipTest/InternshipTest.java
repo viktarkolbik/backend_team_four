42468f6f96e5e72138dec98b50be02cb2783731b
Olga <olgaaevdokim@gmail.com>
2021-05-05 10:54:27 +0300

    edit data inn test after appending  new test import

--- src/test/java/by/exadel/internship/internshipTest/InternshipTest.java
+++ src/test/java/by/exadel/internship/internshipTest/InternshipTest.java
@@ -8,7 +8,6 @@ import by.exadel.internship.dto.internship.GuestShortInternshipDTO;
 import by.exadel.internship.dto.location.CityDTO;
 import by.exadel.internship.dto.location.CountryDTO;
 import by.exadel.internship.dto.location.LocationDTO;
-import by.exadel.internship.entity.location.Location;
 import com.fasterxml.jackson.core.type.TypeReference;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
@@ -41,10 +40,12 @@ public class InternshipTest extends InternshipApplicationTests {
         LocationDTO locationDTO1 = new LocationDTO(new CountryDTO(UUID.fromString("21aab798-2d48-459c-bb84-789d2237f933"), "Belarus"), new CityDTO(UUID.fromString("8c48af72-358a-49d4-a786-ce723eefc384"), "Minsk"));
         LocationDTO locationDTO2 = new LocationDTO(new CountryDTO(UUID.fromString("21aab798-2d48-459c-bb84-789d2237f933"), "Belarus"), new CityDTO(UUID.fromString("b38d8113-d5e8-4967-b8ff-e1df0844790f"), "Gomel"));
         LocationDTO locationDTO3 = new LocationDTO(new CountryDTO(UUID.fromString("de5e7623-c298-4033-8419-9e2fd12afdfa"), "Ukraine"), new CityDTO(UUID.fromString("dceb0743-e262-42b8-9d58-bbdb4e5cd729"), "Kiev"));
-        assertEquals(guestFullInternshipDTO.getName(), "some name");
-        assertEquals(guestFullInternshipDTO.getDescription(), "description1");
-        assertEquals(guestFullInternshipDTO.getRequirements(), "requirements1");
-        assertEquals(guestFullInternshipDTO.getTechSkills(), "skills1");
+        assertEquals(guestFullInternshipDTO.getName(), "JAVA, JS internship");
+        assertEquals(guestFullInternshipDTO.getDescription(), "During the training, you will learn how to write simple and \"clean\" code, create programs that run in a web browser and have web services, develop server-side applications for forums, online stores or surveys, and create multifunctional applications");
+        assertEquals(guestFullInternshipDTO.getRequirements(), "OOP knowledge: classes, encapsulation, polymorphism, inheritance;\n" +
+                "knowledge of the basics of SQL and the basics of database design is desirable;\n" +
+                "knowledge of English at a level not lower than A2;");
+        assertEquals(guestFullInternshipDTO.getTechSkills(), "Docker; GitHub; Swagger UI");
         assertEquals(guestFullInternshipDTO.getSkills(), Set.of(Skill.JAVA,Skill.JS));
         assertEquals(guestFullInternshipDTO.getInternshipFormat(), InternshipFormat.ONLINE);
         assertEquals(guestFullInternshipDTO.getLocations(), List.of(locationDTO1,locationDTO2,locationDTO3));
