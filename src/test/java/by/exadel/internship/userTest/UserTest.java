842f3843fe2041e5dd3575252991f8ebb90487aa
Olga <olgaaevdokim@gmail.com>
2021-05-15 16:22:05 +0300

    edit test

--- src/test/java/by/exadel/internship/userTest/UserTest.java
+++ src/test/java/by/exadel/internship/userTest/UserTest.java
@@ -132,7 +132,7 @@ public class UserTest extends InternshipApplicationTests {
     public void checkWrongData_expect_BadRequestStatus() throws Exception {
         URI uri = UriComponentsBuilder.fromPath("/users/skills")
                 .queryParam("skills", "JAVA,G").build().toUri();
-        MvcResult result = getResult(HttpMethod.GET, uri, status().isBadRequest());
+        getResult(HttpMethod.GET, uri, status().isBadRequest());
 
     }
 
