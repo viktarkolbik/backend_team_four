c6aa49b0c3760653f010a18ee0a61dea0b0df962
Fenydude <bogurov1@gmail.com>
2021-05-17 17:59:50 +0300

    add credential to gitignore, remove unused imports

--- src/main/java/by/exadel/internship/service/CalendarService.java
+++ src/main/java/by/exadel/internship/service/CalendarService.java
@@ -1,7 +1,6 @@
 package by.exadel.internship.service;
 
 import com.google.api.services.calendar.Calendar;
-import org.springframework.stereotype.Service;
 
 public interface CalendarService {
     Calendar getDefaultCalendar();
