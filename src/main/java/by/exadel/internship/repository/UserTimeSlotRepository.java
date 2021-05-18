193737a23a43f2501488fd3b96552dab5c6b85f3
maksim mayeuski <65280608+Maksim-maevsky@users.noreply.github.com>
2021-05-18 22:21:14 +0300

    added @repository

--- src/main/java/by/exadel/internship/repository/UserTimeSlotRepository.java
+++ src/main/java/by/exadel/internship/repository/UserTimeSlotRepository.java
@@ -2,8 +2,10 @@ package by.exadel.internship.repository;
 
 import by.exadel.internship.entity.UserTimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserTimeSlotRepository extends JpaRepository<UserTimeSlot, UUID> {
 }
