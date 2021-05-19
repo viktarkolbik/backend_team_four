7ab893c9b3ad94bb259b2a989912fa334858e717
Fenydude <bogurov1@gmail.com>
2021-05-19 16:35:57 +0300

    remove unused imports

--- src/main/java/by/exadel/internship/controller/InterviewController.java
+++ src/main/java/by/exadel/internship/controller/InterviewController.java
@@ -1,15 +1,16 @@
 package by.exadel.internship.controller;
 
-import by.exadel.internship.annotation.AdminAccessControl;
 import by.exadel.internship.annotation.UserAccessControl;
 import by.exadel.internship.dto.enums.UserRole;
-import by.exadel.internship.dto.interview.InterviewDTO;
 import by.exadel.internship.dto.interview.InterviewInfoDTO;
 import by.exadel.internship.service.InterviewService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
