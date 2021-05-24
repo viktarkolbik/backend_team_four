662519ca8b43d06407074e7e02ec4b421c8f5289
Olga <olgaaevdokim@gmail.com>
2021-05-24 11:35:03 +0300

    delete excessive import

--- src/main/java/by/exadel/internship/repository/UserRepository.java
+++ src/main/java/by/exadel/internship/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package by.exadel.internship.repository;
 
-import by.exadel.internship.dto.enums.Skill;
 import by.exadel.internship.dto.enums.UserRole;
 import by.exadel.internship.entity.User;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -10,10 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 
 @Repository
